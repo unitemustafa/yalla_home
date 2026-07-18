8eaacf4e0a07d24ea368dd0abb137f375933e4ae
uniteMustafa <unitemustafa@gmail.com>
2026-07-18 12:01:23 +0300

    feat: preserve sessions on rate limiting

--- lib/core/network/api_exception.dart
+++ lib/core/network/api_exception.dart
@@ -1,8 +1,15 @@
 class ApiException implements Exception {
-  const ApiException(this.message, {this.statusCode});
+  const ApiException(
+    this.message, {
+    this.statusCode,
+    this.code,
+    this.retryAfterSeconds,
+  });
 
   final String message;
   final int? statusCode;
+  final String? code;
+  final int? retryAfterSeconds;
 
   @override
   String toString() => message;
